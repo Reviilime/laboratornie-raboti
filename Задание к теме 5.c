9da99808ecf5b757b5d8809eb676ccc4d4143832
Reviilime <55348885+Reviilime@users.noreply.github.com>
2020-10-19 16:18:24 +0300

    5

--- Задание к теме 5.c
+++ Задание к теме 5.c
@@ -22,36 +22,72 @@ void findACandBCandSumm()
 {
     system("cls");
     printf("Даны три точки A, B, C на числовой оси. Найти длины отрезков AC и BC и их сумму.\n");
-    float A, B, C = 0;
+    int A, B, C = 0;
     printf("Введите A: ");
-    scanf("%f", &A);
+    scanf("%i", &A);
     printf("Введите B: ");
-    scanf("%f", &B);
+    scanf("%i", &B);
     printf("Введите C: ");
-    scanf("%f", &C);
-    printf("AC = %f\nBC = %f\n", sqrtf((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
-    //TODO
+    scanf("%i", &C);
+    printf("AC = %i\n", abs(C - A));
+    printf("BC = %i\n", abs(C - B));
+    printf("AC + BC = %i\n", abs(C - A) + abs(C - B));
 }
 
 void findACxBC()
 {
     system("cls");
-    printf("To be done\n");
-    //TODO
+    printf("Даны три точки A, B, C на числовой оси. Точка C расположена между точками A и B. Найти произведение длин отрезков AC и BC\n");
+    int A, B, C = 0;
+    printf("Введите A: ");
+    scanf("%i", &A);
+    printf("Введите B: ");
+    scanf("%i", &B);
+    printf("Введите C: ");
+    scanf("%i", &C);
+    printf("AC x BC = %i\n", abs(C - A) * abs(C - B));
 }
 
 void findPandSRectangle()
 {
     system("cls");
-    printf("To be done\n");
-    //TODO
+    printf("Даны координаты двух противоположных вершин прямоугольника (x₁, y₁), (x₂, y₂). Стороны прямоугольника параллельны осям координат. Найти P и S данного прямоугольника.\n");
+    int x1, y1, x2, y2 = 0;
+    printf("Введите x₁: ");
+    scanf("%i", &x1);
+    printf("Введите y₁: ");
+    scanf("%i", &y1);
+    printf("Введите x₂: ");
+    scanf("%i", &x2);
+    printf("Введите y₂: ");
+    scanf("%i", &y2);
+    printf("P = %i\n", (abs(y2 - y1) + abs(x2 - x1)) * 2);
+    printf("S = %i\n", abs(y2 - y1) * abs(x2 - x1));
 }
 
 void findPandSTriangle()
 {
     system("cls");
-    printf("To be done\n");
-    //TODO
+    printf("Даны координаты трех вершин треугольника (x₁, y₁), (x₂, y₂), (x₃, y₃). Найти его P и S\n");
+    float x1, y1, x2, y2, x3, y3, a, b, c, P = 0;
+    printf("Введите x₁: ");
+    scanf("%f", &x1);
+    printf("Введите y₁: ");
+    scanf("%f", &y1);
+    printf("Введите x₂: ");
+    scanf("%f", &x2);
+    printf("Введите y₂: ");
+    scanf("%f", &y2);
+    printf("Введите x₃: ");
+    scanf("%f", &x3);
+    printf("Введите y₃: ");
+    scanf("%f", &y3);
+    a = sqrtf((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    b = sqrtf((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+    c = sqrtf((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+    P = a + b + c;
+    printf("P = %f\n", P);
+    printf("S = %f\n", sqrt(P/2 * (P/2 - a) * (P/2 - b) * (P/2 - c)) );
 }
 
 int main()
